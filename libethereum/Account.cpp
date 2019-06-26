a3ee5c9739a4e9493092f616f0e95c5fd7de0457
Andrei Maiboroda <andrei@ethereum.org>
2019-06-26 13:22:08 +0200

    Implement EIP-1702 Generalized Account Versioning Scheme in LegacyVM

--- libethereum/Account.cpp
+++ libethereum/Account.cpp
@@ -34,11 +34,12 @@ using namespace dev::eth::validation;
 
 namespace fs = boost::filesystem;
 
-void Account::setCode(bytes&& _code)
+void Account::setCode(bytes&& _code, u256 const& _version)
 {
     m_codeCache = std::move(_code);
     m_hasNewCode = true;
     m_codeHash = sha3(m_codeCache);
+    m_version = _version;
 }
 
 void Account::resetCode()
@@ -162,7 +163,7 @@ AccountMap dev::eth::jsonToAccountMap(std::string const& _json, u256 const& _def
                         cerr << "Error importing code of account " << a
                              << "! Code needs to be hex bytecode prefixed by \"0x\".";
                     else
-                        ret[a].setCode(fromHex(codeStr));
+                        ret[a].setCode(fromHex(codeStr), 0);
                 }
                 else
                     cerr << "Error importing code of account " << a
@@ -182,7 +183,7 @@ AccountMap dev::eth::jsonToAccountMap(std::string const& _json, u256 const& _def
                     if (code.empty())
                         cerr << "Error importing code of account " << a << "! Code file "
                              << codePath << " empty or does not exist.\n";
-                    ret[a].setCode(std::move(code));
+                    ret[a].setCode(std::move(code), 0);
                 }
                 else
                     cerr << "Error importing code of account " << a
